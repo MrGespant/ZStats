b068a073f4bdfe3405e1da27d211f92d9dec71d4
Tomas <tomas@meisner.cz>
2018-09-21 21:56:26 +0200

    xlrd test

--- parseXLS.py
+++ parseXLS.py
@@ -1,3 +1,13 @@
 # This script should parse given XLS file and fill the database with included data
 #
 #
+
+import xlrd
+
+path = "./files/investice-TomasMeisner-2018-09-21.xls"
+#TODO soubor jako vstupni parametr
+
+sheet = xlrd.open_workbook(path).sheet_by_index(0)
+#TODO projit soubor a ulozit data jednotlivych pribehu
+
+print(sheet.nrows)

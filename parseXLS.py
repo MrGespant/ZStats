21fc369f2d60f793b58f57e2ecdd1e3e9c5d9b14
Tomas <tomas@meisner.cz>
2018-09-21 23:16:12 +0200

    drive test

--- parseXLS.py
+++ parseXLS.py
@@ -4,6 +4,18 @@
 
 import xlrd
 import sqlite3
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+# you have to get client_secrets.json file from Google API Console and save it in root
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+drive = GoogleDrive(gauth)
+
+file1 = drive.CreateFile({'title': 'Hello.txt'})
+file1.SetContentString('Hello')
+file1.Upload()
 
 path = "./files/investice-TomasMeisner-2018-09-21.xls"
 # TODO soubor jako vstupni parametr

82e1b9cfeba7393df57ea04da7b8316edcab6a6c
Tomas <tomas@meisner.cz>
2018-09-21 22:35:33 +0200

    sqlite3 test

--- parseXLS.py
+++ parseXLS.py
@@ -3,11 +3,27 @@
 #
 
 import xlrd
+import sqlite3
 
 path = "./files/investice-TomasMeisner-2018-09-21.xls"
-#TODO soubor jako vstupni parametr
+# TODO soubor jako vstupni parametr
 
 sheet = xlrd.open_workbook(path).sheet_by_index(0)
-#TODO projit soubor a ulozit data jednotlivych pribehu
+# TODO projit soubor a ulozit data jednotlivych pribehu
 
-print(sheet.nrows)
+try:
+    db = sqlite3.connect("./data.db")
+    # TODO ukladat databazi na Drive
+    cursor = db.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS stories(id INTEGER PRIMARY KEY,sum INTEGER)''')
+    db.commit()
+
+    cursor.execute('''INSERT INTO stories(sum) VALUES(?)''', sheet.nrows)
+    db.commit()
+
+except Exception as e:
+    db.rollback()
+    raise e
+finally:
+    db.close()

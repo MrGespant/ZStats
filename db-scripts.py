2123815b3839ac0030a5d9b78d397b29625f1539
MrGespant <tomas@meisner.cz>
2018-10-17 14:24:12 +0200

    db-init, db-create split

--- db-scripts.py
+++ db-scripts.py
@@ -5,23 +5,24 @@ from sqlite3.dbapi2 import Connection
 from resources.secrets import config
 
 
-def zstats_db_init():
+def db_init():
+    # initialize the connection to DB
     try:
         db: Connection = sqlite3.connect(config.db_database_location)
         # TODO ukladat databazi na Drive
     except Exception as e:
         raise e
-
     print("DB connection established")
-    cursor = db.cursor()
+    return db.cursor()
 
+
+def db_create(cursor):
     try:
         fd = open(config.db_create_script, 'r')
         sqlFile = fd.read()
         fd.close()
     except Exception as e:
         raise e
-
     print("Create script loaded")
     sqlCommands = sqlFile.split(';')
 
@@ -42,4 +43,5 @@ def zstats_db_init():
         db.close()
 
 
-zstats_db_init()
+cursor = db_init()
+db_create(cursor)

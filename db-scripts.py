d06a4cff84cce23212533a35a926ecd629adee49
MrGespant <tomas@meisner.cz>
2018-10-17 17:03:44 +0200

    generalization of DB queries

--- db-scripts.py
+++ db-scripts.py
@@ -8,12 +8,16 @@ from resources.secrets import config
 insert = """INSERT INTO stories (
     id, user_id, name, status, date_start, date_next_payment, date_secondary_bought, date_secondary_sold_date, amount, full_payment, my_payment, installments, installments_when_bought, insurance, postponed, interest_rate, fee, rating
 ) VALUES (
-    1,2,'try','start',NULL,'2018-05-13', NULL, NULL, 10000, 2300, 13.5, 5, NULL, 1, 0, 5.86, 2, 'A++'  
+    2,2,'try','start',NULL,'2018-05-13', NULL, NULL, 10000, 2300, 13.5, 5, NULL, 1, 0, 5.86, 2, 'A++'  
 )"""
 
 
 def db_init():
+    #
     # initialize the connection to DB
+    # returns the DB connector
+    #
+
     try:
         db: Connection = sqlite3.connect(config.db_database_location)
         # TODO ukladat databazi na Drive
@@ -25,24 +29,32 @@ def db_init():
     return db
 
 
-def db_script_open(query):
+def db_script_open(script_file):
+    #
+    # opens the SQL "script_file"
+    # returns the full "script" from file
+    #
+
     try:
-        fd = open(query, 'r')
-        sqlFile = fd.read()
+        fd = open(script_file, 'r')
+        script = fd.read()
         fd.close()
     except Exception as e:
-        print("Script " + query + " not loaded")
+        print("Script from file " + script_file + " not loaded")
         raise e
 
-    print("Script " + query + " loaded")
-    return sqlFile
+    print("Script from file " + script_file + " loaded")
+    return script
 
 
 def db_query(db, script):
+    #
+    # runs the queries in "script" on database "db"
+    #
 
-    sqlCommands = script.split(';')
+    commands = script.split(';')
 
-    for command in sqlCommands:
+    for command in commands:
         try:
             db.cursor().execute(command)
         except Exception as e:
@@ -53,7 +65,7 @@ def db_query(db, script):
         db.commit()
     except Exception as e:
         db.rollback()
-        print("Script rollbacked")
+        print("Script rolled back")
         raise e
 
 
